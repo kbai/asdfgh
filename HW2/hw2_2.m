8cdb51d87e1161855f60d5928b0c452b97135998
Yiran Ma <yiran06@gmail.com>
2015-10-18 00:02:09 -0700

    update hw2-p2

--- HW2/hw2_2.m
+++ HW2/hw2_2.m
@@ -15,28 +15,33 @@ xlabel('x'); ylabel('y');
 % grid search
 m1 = -150:0.1:0;
 m2 = 1:0.01:10;
+
 % L1 norm
 [m1_l1,m2_l1,err_l1,err_all_l1] = grid_search(x,y,m1,m2,1);
+
 % L2 norm
 [m1_l2,m2_l2,err_l2,err_all_l2] = grid_search(x,y,m1,m2,2);
 
 % plotting
 figure(2)
+colormap(jet);
 subplot(121)
-pcolor(m2,m1,err_all_l1); 
+pcolor(m2,m1,err_all_l1);  % error map
 hold on;
-plot(m2_l1,m1_l1,'ko');
+plot(m2_l1,m1_l1,'go');    % optimum solution
 shading flat;
 caxis(crange(err_all_l1));
+colorbar('horiz');
 xlabel('m2');ylabel('m1');title('L1 norm');
 hold off; 
 
 subplot(122)
-pcolor(m2,m1,err_all_l2);
+pcolor(m2,m1,err_all_l2);  % error map
 hold on;
-plot(m2_l2,m1_l2,'ko');
+plot(m2_l2,m1_l2,'go');    % optimum solution
 shading flat;
 caxis(crange(err_all_l2));
+colorbar('horiz');
 xlabel('m2');ylabel('m1');title('L2 norm');
 hold off;
 
@@ -67,7 +72,7 @@ function [m1_best,m2_best,err_best,err] = grid_search(xdata,ydata,m1,m2,flag)
 err = zeros(length(m1),length(m2));
 for i = 1:length(m1)
     for j = 1:length(m2)
-        err(i,j) = misfit(xdata,ydata,m1(i),m2(i),flag);
+        err(i,j) = misfit(xdata,ydata,m1(i),m2(j),flag);
     end
 end
 
@@ -78,7 +83,7 @@ m1_best = m1(I);
 m2_best = m2(J);
 end
 
-% calculate the misfit
+% CALCULATE THE MISFIT
 function err = misfit(xdata,ydata,m1,m2,flag)
 ypred = m1 + m2 * xdata;
 if flag == 1       % L1 norm
