bda33ce2a83d1c3ad5df74a0031df93624f77208
Kangchen Bai <kbai@titaia.gps.caltech.edu>
2015-10-18 17:49:22 -0700

    HW2Q1 finished

--- HW2/compute_gradient_approx_hess.m
+++ HW2/compute_gradient_approx_hess.m
@@ -1,4 +1,4 @@
-function [ Grad,Hess ,Hess_real] = compute_gradient_approx_hess( x,y,M,residue)
+function [ Grad, Hess] = compute_gradient_approx_hess( x,y,M,residue)
 
 xs = M(1);
 ys = M(2);
@@ -10,36 +10,37 @@ R = ((x - xs).^2 + (y - ys).^2 + zs^2);
 dx = x-xs;
 dy = y-ys;
 
-Jacob(:,1) = (3.*p.*zs.*(dx))./(2*(R).^(5/2));
-Jacob(:,2) = (3.*p.*zs.*(dy))./(2*(R).^(5/2));
+Jacob(:,1) = (3.*p.*zs.*(dx))./((R).^(5/2));
+Jacob(:,2) = (3.*p.*zs.*(dy))./((R).^(5/2));
 Jacob(:,3) = p./(R).^(3/2) - (3*p.*zs.^2)./(R).^(5/2);
 Jacob(:,4) = zs./(R).^(3/2);
 
-Grad = 2*(residue')* Jacob;
+Grad = (residue')* Jacob;
 %this is the apprximated Hessian;
 
 
+%dHess = (G(m)-d)^T*Q
+dHess = zeros(4);
+dHess(1,2) = residue'*((15*p*zs*(dx).*(dy))./(4*(R).^(7/2)));
+dHess(1,3) = residue'*((3*p.*(dx))./(2*(R).^(5/2)) - (15*p*zs.^2.*(dx))./(2*(R).^(7/2)));
+dHess(1,4) = residue'*((3*zs.*(dx))./(2*(R).^(5/2)));
+dHess(2,3) = residue'*( (3*p.*(dy))./(2*(R).^(5/2)) - (15*p*zs.^2.*(dy))./(2*(R).^(7/2)));
+dHess(2,4) = residue'*((3*zs.*(dy))./(2*(R).^(5/2)));
+dHess(3,4) = residue'*(1./(R).^(3/2) - (3*zs^2)./(R).^(5/2));
 
-Hess = zeros(4);
-Hess(1,2) =2 * residue'*((15*p*zs*(dx).*(dy))./(4*(R).^(7/2)));
-Hess(1,3) =2 * residue'*((3*p.*(dx))./(2*(R).^(5/2)) - (15*p*zs.^2.*(dx))./(2*(R).^(7/2)));
-Hess(1,4) =2 * residue'*((3*zs.*(dx))./(2*(R).^(5/2)));
-Hess(2,3) =2 * residue'*( (3*p.*(dy))./(2*(R).^(5/2)) - (15*p*zs.^2.*(dy))./(2*(R).^(7/2)));
-Hess(2,4) =2 * residue'*((3*zs.*(dy))./(2*(R).^(5/2)));
-Hess(3,4) =2 * residue'*(1./(R).^(3/2) - (3*zs^2)./(R).^(5/2));
+dHess = (dHess + dHess');
 
-Hess = (Hess + Hess');
-
-Hess(1,1) =2 * residue'*((15*p*zs.*(dx).^2)./(4*(R).^(7/2)) - (3*p.*zs)./(R).^(5/2));
-Hess(2,2) =2 * residue'*((15*p*zs.*(dy).^2)./(4*(R).^(7/2)) - (3*p.*zs)./(R).^(5/2));
-Hess(3,3) =2 * residue'*((15*p*zs^3)./(R).^(7/2) - (9*p*zs)./(R).^(5/2));
-Hess(4,4) = 0;
-
-
-Hess_real = Hess;
+dHess(1,1) = residue'*((15*p*zs.*(dx).^2)./(4*(R).^(7/2)) - (3*p.*zs)./(R).^(5/2));
+dHess(2,2) = residue'*((15*p*zs.*(dy).^2)./(4*(R).^(7/2)) - (3*p.*zs)./(R).^(5/2));
+dHess(3,3) = residue'*((15*p*zs^3)./(R).^(7/2) - (9*p*zs)./(R).^(5/2));
+dHess(4,4) = 0;
 
 
-Hess = 2*(Jacob')*Jacob;
+
+Hess = (Jacob')*Jacob;
+%only add dHess if using exact Hessian
+Hess = Hess+dHess;
+
 Hess = 0.5*(Hess + Hess');
 
 end
