153c0aeffc67291464ab49caa5a86f98daacc668
Kangchen Bai <kbai@titaia.gps.caltech.edu>
2015-10-26 10:06:50 -0700

    HW2 code

--- HW2/nonlinear_solver.m
+++ HW2/nonlinear_solver.m
@@ -1,23 +1,31 @@
 
 function[M]=nonlinear_solver(x,y,d,Minit)
 
-M=Minit;
+M = Minit;
+
+misfit_old = 0;
+
+misfit = 0;
 
 for ii = 1:1:1000
     
-r=compute_residue(x,y,M,d);
+misfit = compute_misfit(x,y,M,d);
 
 
-[Grad,Hess]=compute_gradient_approx_hess(x,y,M,r);
+[Grad,Hess] = compute_gradient_approx_hess(x,y,M,misfit);
 
-deltaM= (Hess)\Grad';
+deltaM= (Hess)\Grad'; 
 
-M=M+deltaM;
+M=M + deltaM;
 
-if (norm(r)<1e-7)
+if (norm(misfit - misfit_old)<1e-7)
     break;
 end
+
+misfit_old = misfit;
 end
+
+disp(['Number of iterations:',num2str(ii)]);
 end 
 
 
