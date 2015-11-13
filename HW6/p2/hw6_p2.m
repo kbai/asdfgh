6a2dbb2a4ea5b9464be1e8bebd8c35796d76ff84
Yiran Ma <yiran06@gmail.com>
2015-11-13 12:08:31 -0800

    p2

--- HW6/p2/hw6_p2.m
+++ HW6/p2/hw6_p2.m
@@ -3,33 +3,49 @@ set(0,'defaulttextfontname','times','defaulttextfontsize',14);
 set(0,'defaultaxesfontname','times','defaultaxesfontsize',14);
 
 
-x1 = [1 -3 4 5];
-x2 = [-0.1 0.3 -0.4 0.5];
-x3 = [101 97 104 105];
+% x{1} = [1 -3 4 5]';
+% x{2} = [-0.1 0.3 -0.4 0.5]';
+% x{3} = [101 97 104 105]';
 
+x{1} = [1 2 3 4]';
+x{2} = x{1} * 0.1;
+x{3} = [10 12 14 16]';
+%x{2} = x{1} * 0.1;
+%x{3} = x{1} * 10;
 
 m1 = 1; m2 = 1;
+for i = 1:3
+    y{i} = m1 + m2.*x{i} + [0;-0.15;0.1;-0.2];
+end
 
-y1 = m1 + m2.*x1 + randn(1,4);
-y2 = m1 + m2.*x2 + randn(1,4);
-y3 = m1 + m2.*x3 + randn(1,4);
 
 figure(1);
-subplot(131)
-plot(x1,y1,'ro'); grid on; xlabel('x');ylabel('y'); axis square;
-subplot(132)
-plot(x2,y2,'go'); grid on; xlabel('x');ylabel('y'); axis square;
-subplot(133)
-plot(x3,y3,'bo'); grid on; xlabel('x');ylabel('y'); axis square;
+for i = 1:3
+    subplot(1,3,i)
+    plot(x{i},y{i},'o');
+    hold on;
+    plot(x{i},m1 + m2.*x{i},'k-');
+    hold all;
+end
 
 %
-G{1} = [ones(4,1) x1(:)];
-G{2} = [ones(4,1) x2(:)];
-G{3} = [ones(4,1) x3(:)];
+for i = 1:3
+    G{i} = [ones(4,1) x{i}];
+end
 
 for i = 1:3
     disp(i);
     [U,SING,V] = svd(G{i})
     [EIGVEC,EIGV] = eig(G{i}'*G{i})
     
+end
+
+
+% test, add same level of error
+
+
+figure(1)
+
+for i = 1:3
+   m = (G{i}'*G{i})^-1*G{i}'*y{i}
 end
