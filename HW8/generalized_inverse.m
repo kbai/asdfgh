aa6c7d4eee47d66af2f05ac1d2fcc7ece94e0620
Kangchen Bai <kangchenbai@gmail.com>
2015-12-01 14:07:45 -0800

    HW8 initiated

---
+++ HW8/generalized_inverse.m
@@ -0,0 +1,24 @@
+function [model,Resolution] = generalized_inverse(data,GG,p)
+
+if nargin <= 2  % if p is not set, do not truncate
+    p = min(size(GG));
+end
+
+[UU,DD,VV] = svd(GG);
+
+I_DD = zeros(size(DD'));
+
+for tmp = 1:1:p 
+    
+I_DD(tmp,tmp) = 1/DD(tmp,tmp); 
+
+end
+
+
+model = VV*I_DD*UU'*data;
+
+Resolution = VV*I_DD*DD*VV';
+
+end
+
+
