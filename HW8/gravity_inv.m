0353618ea807cf75605f531ea619a31bff29af12
Yiran Ma <yiran06@gmail.com>
2015-12-05 23:09:34 -0800

    need more modifications...

--- HW8/gravity_inv.m
+++ HW8/gravity_inv.m
@@ -87,13 +87,13 @@ ne=[];
 p=10;
 figure(4)
 %for p = 10:5:20
- for p = 5:30   
-[GI_Dm_trunc,Rg{p}] = generalized_inverse(di,GG,p);   %generalized inverse with truncation
-plot(xi,GI_Dm_trunc);
-hold on
-ne = [ne,norm(di-GG*GI_Dm_trunc)];
-nm = [nm,norm(GI_Dm_trunc)];
- end
+for p = 5:30
+    [GI_Dm_trunc,Rg{p}] = generalized_inverse(di,GG,p);   %generalized inverse with truncation
+    plot(xi,GI_Dm_trunc);
+    hold on
+    ne = [ne,norm(di-GG*GI_Dm_trunc)];
+    nm = [nm,norm(GI_Dm_trunc)];
+end
 
 figure(20);
 loglog(nm,ne,'-o'); 
@@ -104,75 +104,181 @@ hold on
 xlim([3150,3450]);
 ylim([0.1345,0.1395]);
 print('figure/g_trunc_zoomin.png','-dpng');
-pause
 
 %% part (h)
-
-
 nm=[];
 ne=[];
 alpha_all = 10.^[-10:-1];
 for i= 1:length(alpha_all);
-alpha_s = alpha_all(i);
-Tik_Dm = (GG'*GG + alpha_s^2 * eye(Lm))\(GG'*di);
-nm = [nm,norm(Tik_Dm)];
-ne = [ne,norm(GG*Tik_Dm-di)];
-
-figure(5)
-plot(xi,Tik_Dm);
-hold on
-Rt{i} = (GG'*GG + alpha_s^2 * eye(Lm))\(GG'*GG);
+    Tik_Dm = (GG'*GG + alpha_all(i)^2 * eye(Lm))\(GG'*di);
+    nm = [nm,norm(Tik_Dm)];
+    ne = [ne,norm(GG*Tik_Dm-di)];
+    
+    if i>=5 && i<= 7
+        figure(5)
+        if i == 5
+            plot(xi,Tik_Dm,'k-');
+        elseif i == 6
+            plot(xi,Tik_Dm,'r-');
+        elseif i == 7
+            plot(xi,Tik_Dm,'b-');
+        end
+        hold on
+        ylim([-1000 1000]);
+    end
+    Rt{i} = (GG'*GG + alpha_all(i)^2 * eye(Lm))\(GG'*GG);
 end
+xlabel('x (m)'); ylabel('\Deltamkg');
+legend('\alpha=1e-5','\alpha=1e-4','\alpha=1e-3');
+print('figure/h_solutions.png','-dpng');
 
 
 figure(6)
 loglog(ne,nm,'o-');
-loglog(ne(5),nm(5));
-
+hold on;
+plot(ne(6),nm(6),'r*');
 xlabel('Residual norm |Gm-d|_2');
-ylabel('Solution norm |m|_2')
+ylabel('Solution norm |m|_2');
+hold off;
+title('find alpha - 0 order');
+print('figure/h_alphas.png','-dpng');
 
 %% part(i)
-
+% first order Tikhonov
 L1 = diag(ones(1,Lm),0) + diag(-1*ones(1,Lm-1),1);
 L1(end,:) = [];
 
 
-alpha = 1e-5;
+% find the best alpha
+alpha_all = 10.^[-10:-1];
+nm = [];
+ne = [];
+for i = 1:length(alpha_all)
+    Tik_Dm = (GG'*GG + alpha_all(i)^2 * (L1'*L1))\(GG'*di);
+    nm = [nm,norm(L1*Tik_Dm)];
+    ne = [ne,norm(GG*Tik_Dm-di)];
+    Rt{i} = (GG'*GG + alpha_all(i)^2 * (L1'*L1))\(GG'*GG); 
+end
+figure(31);
+subplot(121)
+loglog(ne,nm,'o-');
+hold on;
+plot(ne(7),nm(7),'r*');
+text(ne(7),nm(7),'\alpha=1e-4');
+title('find alpha - first order');
+xlabel('Residual norm |Gm-d|_2');
+ylabel('Solution norm |Lm|_2');
 
+
+alpha = 1e-4;
 Tik1_Dm = (GG'*GG + alpha^2 * (L1'*L1))\(GG'*di);
+Ri1 = (GG'*GG + alpha^2 * (L1'*L1))\(GG'*GG);   % resolution matrix
 
-Ri1 = (GG'*GG + alpha^2 * (L1'*L1))\(GG'*GG);
+
 
 figure(7)
+plot(xi,Tik1_Dm,'k-');
 
-plot(xi,Tik1_Dm);
+% second order Tikhonov
 
 L2 = diag(ones(1,Lm),0) + diag(-2*ones(1,Lm-1),1) + diag(ones(1,Lm-2),2);
 L2(end,:) = [];
 L2(end,:) = [];
 
+% find the best alpha
+alpha_all = 10.^[-10:-1];
+nm = [];
+ne = [];
+for i = 1:length(alpha_all)
+    Tik_Dm = (GG'*GG + alpha_all(i)^2 * (L2'*L2))\(GG'*di);
+    nm = [nm,norm(L2*Tik_Dm)];
+    ne = [ne,norm(GG*Tik_Dm-di)];
+end
+
+figure(31);
+subplot(122)
+loglog(ne,nm,'o-');
+hold on;
+plot(ne(8),nm(8),'r*');
+text(ne(8),nm(8),'\alpha=1e-3');
+title('find alpha - second order');
+xlabel('Residual norm |Gm-d|_2');
+ylabel('Solution norm |Lm|_2');
+print('figure/i_alphas.png','-dpng');
+
+alpha = 1e-3;
 Tik2_Dm = (GG'*GG + alpha^2 * (L2'*L2))\(GG'*di);
 
-Ri2 = (GG'*GG + alpha^2 * (L2'*L2))\(GG'*GG);
+Ri2 = (GG'*GG + alpha^2 * (L2'*L2))\(GG'*GG);  % resolution matrix
 
+figure(7)
 hold on
+plot(xi,Tik2_Dm,'r-');
+xlabel('x (m)'); ylabel('\Deltamkg');
+legend('first-order','second-order');
+print('figure/i_solution.png','-dpng');
 
-plot(xi,Tik2_Dm);
+
+figure
+plot(xd,di,'ko-');
+hold on;
+plot(xd,GG * Tik1_Dm,'r-');
+plot(xd,GG * Tik2_Dm,'b-');
+xlabel('x (m)'); ylabel('\muGal');
+hold off;
+legend('data','first order','second order');
+print('figure/i_pred.png','-dpng');
+
 %% part(j)
-figure(12)
-imagesc(Re_LLS);
-figure(13)
-imagesc(Rf);
-figure(14)
+figure
+imagesc(Re_LLS); colorbar('vert');
+title('(e) Least square');
+print('figure/j1.png','-dpng');
 
-imagesc(Rg{11});
-figure(15)
+figure
+imagesc(Rf); colorbar('vert');
+title('(f) Generalized inverse');
+print('figure/j2.png','-dpng');
 
-imagesc(Rg{16});
-figure(16)
+figure
+imagesc(Rg{11}); colorbar('vert');
+title('(g) p = 11');
+print('figure/j3.png','-dpng');
 
-imagesc(Rg{21});
+figure
+imagesc(Rg{16}); colorbar('vert');
+title('(g) p = 16');
+print('figure/j4.png','-dpng');
+
+figure
+imagesc(Rg{21}); colorbar('vert');
+title('(g) p = 21');
+print('figure/j5.png','-dpng');
+
+figure
+imagesc(Rt{6}); colorbar('vert');
+title('\alpha = 1e-5');
+print('figure/j6.png','-dpng');
+
+figure
+imagesc(Rt{7}); colorbar('vert');
+title('\alpha = 1e-4');
+print('figure/j7.png','-dpng');
+
+figure
+imagesc(Rt{8}); colorbar('vert');
+title('\alpha = 1e-3');
+print('figure/j8.png','-dpng');
+
+figure
+imagesc(Ri1); colorbar('vert');
+title('(i) first-order');
+print('figure/j9.png','-dpng');
+
+figure
+imagesc(Ri2); colorbar('vert');
+title('(i) second-order');
+print('figure/j10.png','-dpng');
 
 
 
